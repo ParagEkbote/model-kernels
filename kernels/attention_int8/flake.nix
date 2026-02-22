bfeb9cb7a61369951d21dc2480a3318c9554e498
Parag Ekbote <thecoolekbote189@gmail.com>
2026-02-22 17:46:30 +0000

    update.

--- kernels/attention_int8/flake.nix
+++ kernels/attention_int8/flake.nix
@@ -1,28 +1,16 @@
-{
-  description = "INT8 Attention Kernel";
+  description = "Flake for kernel";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";  # stable channel
-    kernel-builder.url = "github:huggingface/kernel-builder";
+    kernel-builder.url = "path:../../..";
   };
 
-  outputs = { self, nixpkgs, kernel-builder }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-
-    # Force Python 3.11 instead of default 3.13
-    python = pkgs.python311;
-
-  in
-    kernel-builder.lib.genFlakeOutputs {
+  outputs =
+    {
+      self,
+      kernel-builder,
+    }:
+    kernel-builder.lib.genKernelFlakeOutputs {
       inherit self;
       path = ./.;
-
-      # override python used by kernel-builder
-      python = python;
     };
 }
